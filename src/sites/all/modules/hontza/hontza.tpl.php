56d2604394d688791c4e8a32b077e16fd8774830
imanolhontza <imanol@hontza.es>
2016-08-24 12:04:42 +0200

    Add files via upload
    Update

--- src/sites/all/modules/hontza/hontza.tpl.php
+++ src/sites/all/modules/hontza/hontza.tpl.php
@@ -4,7 +4,7 @@
 ?>
 <?php global $user;?>
 <?php if ($user->name):?>
-<div class="menu-user"><li style="float:left;padding-left:0px;"><?php print red_funciones_get_actualizacion_activado_menu_icono();?><?php print red_funciones_get_subdominio_activado_menu_icono();?><?php print red_funciones_get_language_menu_icono();?><?php print get_language_selection_li();?></li><li class="user-m" style="float:left;background-position: top left;"><?php print red_funciones_get_user_menu_icono();?><?php print hontza_define_user_menu_input_select().hontza_get_rol_base_user($user).'</li>'.red_funciones_define_user_menu_net_resources_input_select_html().red_funciones_get_gestion_menu_icono().hontza_define_user_menu_management_input_select_html();?></div>
+<div class="menu-user"><li style="float:left;padding-left:0px;"><?php print red_funciones_get_actualizacion_activado_menu_icono();?><?php print red_visualizador_get_visualizador_activado_menu_icono();?><<?php print red_funciones_get_subdominio_activado_menu_icono();?><?php print red_funciones_get_language_menu_icono();?><?php print get_language_selection_li();?></li><li class="user-m" style="float:left;background-position: top left;"><?php print red_funciones_get_user_menu_icono();?><?php print hontza_define_user_menu_input_select().hontza_get_rol_base_user($user).'</li>'.red_funciones_define_user_menu_net_resources_input_select_html().red_funciones_get_gestion_menu_icono().hontza_define_user_menu_management_input_select_html();?></div>
   <?php else: ?>
     <?php //intelsat-2015 ?>
     <?php if(hontza_canal_rss_is_visualizador_activado()):?>
