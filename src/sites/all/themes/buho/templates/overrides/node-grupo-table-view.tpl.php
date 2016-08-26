19015ed8ee5fd6a9bf09d0fe5ace74de1aa19480
imanolhontza <imanol@hontza.es>
2016-08-26 11:45:58 +0200

    Add files via upload
    Update

--- src/sites/all/themes/buho/templates/overrides/node-grupo-table-view.tpl.php
+++ src/sites/all/themes/buho/templates/overrides/node-grupo-table-view.tpl.php
@@ -12,11 +12,26 @@
     <tr class="tr_node_view">
         <td class="td_label_node_view"><b><?php print t('Summary');?></b>:&nbsp;</td>
         <td class="td_value_node_view"><?php print $node->og_description;?></td>
-    </tr>    				
-    <tr class="tr_node_view">
-        <td class="td_label_node_view"><b><?php print t('Subject Area');?></b>:&nbsp;</td>
-        <td class="td_value_node_view"><?php print $node->field_tematica[0]['value'];?></td>
     </tr>
+    <?php $info_tags=hontza_grupos_mi_grupo_get_tags_fields('',$node);?>
+    <?php if(isset($info_tags['tags']) && !empty($info_tags['tags'])):?>
+    <tr class="tr_node_view">
+        <td class="td_label_node_view"><b><?php print t('Thematic Tags');?></b>:&nbsp;</td>
+        <td class="td_value_node_view"><?php print $info_tags['tags'];?></td>
+    </tr>
+    <?php endif;?>
+    <?php if(isset($info_tags['tags_geograficos']) && !empty($info_tags['tags_geograficos'])):?>
+    <tr class="tr_node_view">
+        <td class="td_label_node_view"><b><?php print t('Geographic Tags');?></b>:&nbsp;</td>
+        <td class="td_value_node_view"><?php print $info_tags['tags_geograficos'];?></td>
+    </tr>
+    <?php endif;?>
+    <!--
+    <tr class="tr_node_view">
+        <td class="td_label_node_view"><b><?php //print t('Subject Area');?></b>:&nbsp;</td>
+        <td class="td_value_node_view"><?php //print $node->field_tematica[0]['value'];?></td>
+    </tr>
+    -->
     <tr class="tr_node_view">
         <td class="td_label_node_view"><b><?php print t('Type of group');?></b>:&nbsp;</td>
         <td class="td_value_node_view"><?php print hontza_grupos_mi_grupo_get_type_of_group_string($node);?></td>
@@ -39,18 +54,33 @@
     </tr>
     <?php if(red_funciones_is_administrador_grupo()):?>
     <tr class="tr_node_view">
-        <td class="td_label_node_view"><b><?php print t('Alchemy key');?></b>:&nbsp;</td>
+        <td class="td_label_node_view"><b><?php print t('Alchemy Api Key');?></b>:&nbsp;</td>
         <td class="td_value_node_view"><?php print $node->field_alchemy_key[0]['value']; ?></td>
     </tr>  
     <tr class="tr_node_view">
-        <td class="td_label_node_view"><b><?php print t('Opencalais key');?></b>:&nbsp;</td>
+        <td class="td_label_node_view"><b><?php print t('Open Calais Api Key');?></b>:&nbsp;</td>
         <td class="td_value_node_view"><?php print $node->field_opencalais_key[0]['value']; ?></td>
-    </tr>											  							
+    </tr>    
+    <?php //intelsat-2016?>
+    <!--
+    <tr class="tr_node_view">
+        <td class="td_label_node_view"><b><?php //print 'Kimonolabs Api Key';?></b>:&nbsp;</td>
+        <td class="td_value_node_view"><?php //print $node->field_grupo_kimonolabs_api_key[0]['value']; ?></td>
+    </tr>
+    -->
     <?php endif;?>    				
     <tr class="tr_node_view">
         <td class="td_label_node_view"><b><?php print t('Active tabs');?></b>:&nbsp;</td>
-        <td class="td_value_node_view"><?php print hontza_grupos_get_active_tabs_html($node); ?></td>
-    </tr>    				
+        <td class="td_value_node_view"><?php print hontza_grupos_get_active_tabs_html($node);?></td>
+    </tr>
+    <tr class="tr_node_view">
+        <td class="td_label_node_view"><b><?php print t('Delete rejected news after');?></b>:&nbsp;</td>
+        <td class="td_value_node_view"><?php print hontza_grupos_mi_grupo_get_field_delete_rejected_news_time_html($node); ?></td>
+    </tr>
+    <tr class="tr_node_view">
+        <td class="td_label_node_view"><b><?php print t('Delete unread news after');?></b>:&nbsp;</td>
+        <td class="td_value_node_view"><?php print hontza_grupos_mi_grupo_get_field_delete_unread_news_time_html($node); ?></td>
+    </tr>            
     <tr class="tr_node_view">
         <td class="td_label_node_view"><b><?php print t('Active channels');?></b>:&nbsp;</td>
         <td class="td_value_node_view"><?php print hontza_grupos_get_activate_channels_html($node); ?></td>
@@ -58,5 +88,11 @@
     <tr class="tr_node_view">
         <td class="td_label_node_view"><b><?php print t('Network connected');?></b>:&nbsp;</td>
         <td class="td_value_node_view"><?php print hontza_grupos_get_network_connected_html($node); ?></td>
-    </tr>                            
+    </tr>
+    <?php if(red_dashboard_is_activado()):?>
+    <tr class="tr_node_view">
+        <td class="td_label_node_view"><b><?php print t('Dashboard type');?></b>:&nbsp;</td>
+        <td class="td_value_node_view"><?php print red_dashboard_get_grupo_dashboard_type_label($node); ?></td>
+    </tr>        
+    <?php endif;?>     
 </table>
