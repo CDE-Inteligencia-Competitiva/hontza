e05f9f749f2a535f51c08fd499098a65b17e5b58
imanolhontza <imanol@hontza.es>
2016-11-25 16:52:46 +0100

    Add files via upload
    Update

--- src/sites/all/modules/red/red.informatica.inc.php
+++ src/sites/all/modules/red/red.informatica.inc.php
@@ -26,12 +26,24 @@ function red_informatica_is_mcapi_my_send_mail(){
 	return 0;	
 }
 function red_informatica_my_send_mail($user_mail,$subject,$content,$row,$por_correo){
+	$is_mcapi=0;
 	if(red_informatica_is_mcapi_my_send_mail()){
-    	$mail_array=array();
-    	//echo print_r($row,1);
-    	//exit();
-        boletin_report_mcapi_my_send_mail_array($mail_array,$subject, $content,$row->send_method,$row->is_recibir,0,0,$row,$por_correo);
-    }else{
-    	my_send_mail($user_mail,$subject, $content,$row->send_method,$row->is_recibir);
+		if(isset($row->mailchimp_list_id) && !empty($row->mailchimp_list_id)){
+			$is_mcapi=1;
+	    	$mail_array=array();
+	    	//echo print_r($row,1);
+	    	//exit();
+	    	//$is_recibir=$row->is_recibir;
+	    	$is_recibir=1;
+	        boletin_report_mcapi_my_send_mail_array($mail_array,$subject, $content,$row->send_method,$is_recibir,0,0,$row,$por_correo);
+	    }    
     }
+    if(!$is_mcapi){
+    	my_send_mail($user_mail,$subject, $content,$row->send_method,$row->is_recibir);
+    }	
+}
+function red_informatica_boletin_grupo_save_mailchimp_fields($my_id,$values){
+	if(red_informatica_is_informatica_activado()){
+		informatica_boletin_grupo_save_mailchimp_fields($my_id,$values);
+	}	
 }
